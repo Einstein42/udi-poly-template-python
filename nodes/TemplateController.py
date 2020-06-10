a4d95ee923c837dc357b398da41605ae2d8de227
JimBo CA <jimboca3@gmail.com>
2020-06-10 16:48:45 -0700

    Major reorg on how nodes are stored in files, added examples for logger

---
+++ nodes/TemplateController.py
@@ -0,0 +1,283 @@
+
+
+"""
+Get the polyinterface objects we need.  Currently Polyglot Cloud uses
+a different Python module which doesn't have the new LOG_HANDLER functionality
+"""
+try:
+    from polyinterface import Controller,LOG_HANDLER,LOGGER
+except ImportError:
+    from pgc_interface import Controller,LOGGER
+import logging
+
+# My Template Node
+from nodes import TemplateNode
+
+# IF you want a different log format than the current default
+LOG_HANDLER.set_log_format('%(asctime)s %(threadName)-10s %(name)-18s %(levelname)-8s %(module)s:%(funcName)s: %(message)s')
+
+class TemplateController(Controller):
+    """
+    The Controller Class is the primary node from an ISY perspective. It is a Superclass
+    of polyinterface.Node so all methods from polyinterface.Node are available to this
+    class as well.
+
+    Class Variables:
+    self.nodes: Dictionary of nodes. Includes the Controller node. Keys are the node addresses
+    self.name: String name of the node
+    self.address: String Address of Node, must be less than 14 characters (ISY limitation)
+    self.polyConfig: Full JSON config dictionary received from Polyglot for the controller Node
+    self.added: Boolean Confirmed added to ISY as primary node
+    self.config: Dictionary, this node's Config
+
+    Class Methods (not including the Node methods):
+    start(): Once the NodeServer config is received from Polyglot this method is automatically called.
+    addNode(polyinterface.Node, update = False): Adds Node to self.nodes and polyglot/ISY. This is called
+        for you on the controller itself. Update = True overwrites the existing Node data.
+    updateNode(polyinterface.Node): Overwrites the existing node data here and on Polyglot.
+    delNode(address): Deletes a Node from the self.nodes/polyglot and ISY. Address is the Node's Address
+    longPoll(): Runs every longPoll seconds (set initially in the server.json or default 10 seconds)
+    shortPoll(): Runs every shortPoll seconds (set initially in the server.json or default 30 seconds)
+    query(): Queries and reports ALL drivers for ALL nodes to the ISY.
+    getDriver('ST'): gets the current value from Polyglot for driver 'ST' returns a STRING, cast as needed
+    runForever(): Easy way to run forever without maxing your CPU or doing some silly 'time.sleep' nonsense
+                  this joins the underlying queue query thread and just waits for it to terminate
+                  which never happens.
+    """
+    def __init__(self, polyglot):
+        """
+        Optional.
+        Super runs all the parent class necessities. You do NOT have
+        to override the __init__ method, but if you do, you MUST call super.
+        """
+        super(TemplateController, self).__init__(polyglot)
+        self.name = 'Template Controller'
+        self.hb = 0
+        # This can be used to call your function everytime the config changes
+        # But currently it is called many times, so not using.
+        #self.poly.onConfig(self.process_config)
+
+    def start(self):
+        """
+        Optional.
+        Polyglot v2 Interface startup done. Here is where you start your integration.
+        This will run, once the NodeServer connects to Polyglot and gets it's config.
+        In this example I am calling a discovery method. While this is optional,
+        this is where you should start. No need to Super this method, the parent
+        version does nothing.
+        """
+        # This grabs the server.json data and checks profile_version is up to
+        # date based on the profile_version in server.json as compared to the
+        # last time run which is stored in the DB.  When testing just keep
+        # changing the profile_version to some fake string to reload on restart
+        # Only works on local currently..
+        serverdata = self.poly.get_server_data(check_profile=None)
+        #serverdata['version'] = "testing"
+        LOGGER.info('Started Template NodeServer {}'.format(serverdata['version']))
+        # Show values on startup if desired.
+        LOGGER.debug('ST=%s',self.getDriver('ST'))
+        self.setDriver('ST', 1)
+        self.heartbeat(0)
+        self.check_params()
+        self.set_debug_level(self.getDriver('GV1'))
+        self.discover()
+        self.poly.add_custom_config_docs("<b>This is some custom config docs data</b>")
+
+    def shortPoll(self):
+        """
+        Optional.
+        This runs every 10 seconds. You would probably update your nodes either here
+        or longPoll. No need to Super this method the parent version does nothing.
+        The timer can be overriden in the server.json.
+        """
+        LOGGER.debug('shortPoll')
+        for node in self.nodes:
+            if node != self.address:
+                self.nodes[node].shortPoll()
+
+    def longPoll(self):
+        """
+        Optional.
+        This runs every 30 seconds. You would probably update your nodes either here
+        or shortPoll. No need to Super this method the parent version does nothing.
+        The timer can be overriden in the server.json.
+        """
+        LOGGER.debug('longPoll')
+        self.heartbeat()
+
+    def query(self,command=None):
+        """
+        Optional.
+        By default a query to the control node reports the FULL driver set for ALL
+        nodes back to ISY. If you override this method you will need to Super or
+        issue a reportDrivers() to each node manually.
+        """
+        self.check_params()
+        for node in self.nodes:
+            self.nodes[node].reportDrivers()
+
+    def heartbeat(self):
+        """
+        Optional.
+        Sends a DON/DOF called by start and longPoll which allow users to
+        monitor that the nodeserver is still running
+        """
+        if self.hb == 0:
+            self.reportCmd("DON",2)
+            self.hb = 1
+        else:
+            self.reportCmd("DOF",2)
+            self.hb = 0
+
+    def discover(self, *args, **kwargs):
+        """
+        Example
+        Do discovery here. Does not have to be called discovery. Called from example
+        controller start method and from DISCOVER command recieved from ISY as an exmaple.
+        """
+        self.addNode(TemplateNode(self, self.address, 'templateaddr', 'Template Node Name'))
+
+    def delete(self):
+        """
+        Example
+        This is sent by Polyglot upon deletion of the NodeServer. If the process is
+        co-resident and controlled by Polyglot, it will be terminiated within 5 seconds
+        of receiving this message.
+        """
+        LOGGER.info('Oh God I\'m being deleted. Nooooooooooooooooooooooooooooooooooooooooo.')
+
+    def stop(self):
+        LOGGER.debug('NodeServer stopped.')
+
+    def process_config(self, config):
+        # this seems to get called twice for every change, why?
+        # What does config represent?
+        LOGGER.info("process_config: Enter config={}".format(config));
+        LOGGER.info("process_config: Exit");
+
+    def heartbeat(self,init=False):
+        LOGGER.debug('heartbeat: init={}'.format(init))
+        if init is not False:
+            self.hb = init
+        LOGGER.debug('heartbeat: hb={}'.format(self.hb))
+        if self.hb == 0:
+            self.reportCmd("DON",2)
+            self.hb = 1
+        else:
+            self.reportCmd("DOF",2)
+            self.hb = 0
+
+    def set_module_logs(self,level):
+        logging.getLogger('urllib3').setLevel(level)
+
+    def set_debug_level(self,level):
+        LOGGER.debug('set_debug_level: {}'.format(level))
+        if level is None:
+            level = 30
+        level = int(level)
+        if level == 0:
+            level = 30
+        LOGGER.info('set_debug_level: Set GV1 to {}'.format(level))
+        self.setDriver('GV1', level)
+        # 0=All 10=Debug are the same because 0 (NOTSET) doesn't show everything.
+        if level <= 10:
+            LOGGER.setLevel(logging.DEBUG)
+        elif level == 20:
+            LOGGER.setLevel(logging.INFO)
+        elif level == 30:
+            LOGGER.setLevel(logging.WARNING)
+        elif level == 40:
+            LOGGER.setLevel(logging.ERROR)
+        elif level == 50:
+            LOGGER.setLevel(logging.CRITICAL)
+        else:
+            LOGGER.debug("set_debug_level: Unknown level {}".format(level))
+        # this is the best way to control logging for modules, so you can
+        # still see warnings and errors
+        #if level < 10:
+        #    self.set_module_logs(logging.DEBUG)
+        #else:
+        #    # Just warnigns for the modules unless in module debug mode
+        #    self.set_module_logs(logging.WARNING)
+        # Or you can do this and you will never see mention of module logging
+        if level < 10:
+            LOG_HANDLER.set_basic_config(True,logging.DEBUG)
+        else:
+            # This is the polyinterface default
+            LOG_HANDLER.set_basic_config(True,logging.WARNING)
+
+    def check_params(self):
+        """
+        This is an example if using custom Params for user and password and an example with a Dictionary
+        """
+        self.removeNoticesAll()
+        self.addNotice('Hey there, my IP is {}'.format(self.poly.network_interface['addr']),'hello')
+        self.addNotice('Hello Friends! (without key)')
+        default_user = "YourUserName"
+        default_password = "YourPassword"
+        if 'user' in self.polyConfig['customParams']:
+            self.user = self.polyConfig['customParams']['user']
+        else:
+            self.user = default_user
+            LOGGER.error('check_params: user not defined in customParams, please add it.  Using {}'.format(self.user))
+            st = False
+
+        if 'password' in self.polyConfig['customParams']:
+            self.password = self.polyConfig['customParams']['password']
+        else:
+            self.password = default_password
+            LOGGER.error('check_params: password not defined in customParams, please add it.  Using {}'.format(self.password))
+            st = False
+        # Make sure they are in the params
+        self.addCustomParam({'password': self.password, 'user': self.user, 'some_example': '{ "type": "TheType", "host": "host_or_IP", "port": "port_number" }'})
+
+        # Add a notice if they need to change the user/password from the default.
+        if self.user == default_user or self.password == default_password:
+            # This doesn't pass a key to test the old way.
+            self.addNotice('Please set proper user and password in configuration page, and restart this nodeserver')
+        # This one passes a key to test the new way.
+        self.addNotice('This is a test','test')
+
+    def remove_notice_test(self,command):
+        LOGGER.info('remove_notice_test: notices={}'.format(self.poly.config['notices']))
+        # Remove all existing notices
+        self.removeNotice('test')
+
+    def remove_notices_all(self,command):
+        LOGGER.info('remove_notices_all: notices={}'.format(self.poly.config['notices']))
+        # Remove all existing notices
+        self.removeNoticesAll()
+
+    def update_profile(self,command):
+        LOGGER.info('update_profile:')
+        st = self.poly.installprofile()
+        return st
+
+    def cmd_set_debug_mode(self,command):
+        val = int(command.get('value'))
+        LOGGER.debug("cmd_set_debug_mode: {}".format(val))
+        self.set_debug_level(val)
+
+    """
+    Optional.
+    Since the controller is the parent node in ISY, it will actual show up as a node.
+    So it needs to know the drivers and what id it will use. The drivers are
+    the defaults in the parent Class, so you don't need them unless you want to add to
+    them. The ST and GV1 variables are for reporting status through Polyglot to ISY,
+    DO NOT remove them. UOM 2 is boolean.
+    The id must match the nodeDef id="controller"
+    In the nodedefs.xml
+    """
+    id = 'controller'
+    commands = {
+        'QUERY': query,
+        'DISCOVER': discover,
+        'UPDATE_PROFILE': update_profile,
+        'REMOVE_NOTICES_ALL': remove_notices_all,
+        'REMOVE_NOTICE_TEST': remove_notice_test,
+        'SET_DM': cmd_set_debug_mode,
+    }
+    drivers = [
+        {'driver': 'ST', 'value': 1, 'uom': 2},
+        {'driver': 'GV1', 'value': 30, 'uom': 25}, # Debug (Log) Mode, default=30=Warning
+    ]
---
+++ nodes/__init__.py
@@ -0,0 +1,5 @@
+
+""" Node classes used by the Wireless Sensor Tags Node Server. """
+
+from .TemplateNode            import TemplateNode
+from .TemplateController      import TemplateController
